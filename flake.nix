3add715abb213222fd022f68fd0b860f5cc1bd3c
EllieBytes <jellie7118@proton.me>
2026-04-23 13:41:29 +0000

    Refactor flake.nix to simplify outputs structure

--- flake.nix
+++ flake.nix
@@ -3,13 +3,7 @@
     A flake-parts module for managing hosts & user configurations automatically. 
   '';
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
-
-  outputs = inputs@{ flake-parts, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; } ({ ... }: {
-    flake.flakeModules.default = ./flake-modules/hostess.nix;
+  outputs = { ... }:
+    flakeModule = ./flake-modules/hostess.nix;
   });
 }
